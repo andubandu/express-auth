051f458564f9f5d07c601200f5720fecf8a224ec
andu <lolandu575@gmail.com>
2025-03-15 13:17:20 -0230

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -32,13 +32,15 @@ router.get('/:username', async (req, res) => {
 
 router.delete('/del/:id', authenticateToken, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
+    const user = await User.findOneAndDelete({ _id: req.params.id }); 
     if (!user) return res.status(404).json({ error: 'User not found' });
+
     res.status(204).send();
   } catch (error) {
     res.status(500).json({ error: 'Error deleting user' });
   }
 });
+
 
 router.put(
   '/upd/:id',
