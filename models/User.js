976ffe862c85c2e92a910a1590cae3dd0c3f7422
andu <lolandu575@gmail.com>
2025-03-15 13:15:05 -0230

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import Post from './Post.js'; 
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   avatar: { type: String },
   createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.pre('findOneAndDelete', async function (next) {
+  const userId = this.getQuery()._id;
+  await Post.deleteMany({ author: userId }); 
+  next();
 });
 
 export default mongoose.model('User', userSchema);
